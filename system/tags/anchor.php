91d67f9458539fcbaaf94daec9485cf0c0db6481
Joël Gähwiler <joel.gaehwiler@bluewin.ch>
2011-11-10 10:50:02 +0100

    fixed a bug in anchor tag

--- system/tags/anchor.php
+++ system/tags/anchor.php
@@ -11,8 +11,12 @@ class Anchor_Tag {
 	
 	public static function call($attributes) {
 		$url = Registry::get("base.path").$attributes[1];
+		$actual_path = Registry::get("request.path");
+		if($actual_path = "/") {
+			$actual_path = "";
+		}
 		$options = array();
-		if(Registry::get("request.path")==$attributes[1]) {
+		if($actual_path==$attributes[1]) {
 			$options["class"] = $attributes[2];
 		}
 		return HTML::anchor($url,$attributes[0],$options);
